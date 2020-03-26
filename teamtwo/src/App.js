f2b810c32a8f802c98e3b3a4b92223a63848af9f
Chris Kraft <kriggity@gmail.com>
2020-03-26 17:15:55 -0400

    basic scaffolding

--- teamtwo/src/App.js
+++ teamtwo/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { geolocated } from "react-geolocated";
 import './App.css';
+import Nasa from './components/Nasa';
+import Restaurant from './components/Restaurant';
+import Weather from './components/Weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return !this.props.isGeolocationAvailable ? (
+      <div>Your browser does not support Geolocation</div>
+    ) : !this.props.isGeolocationEnabled ? (
+      <div>Geolocation is not enabled</div>
+    ) : this.props.coords ? (
+      <div>
+        <Nasa coords={this.props.coords} />
+        <Restaurant coords={this.props.coords} />
+        <Weather coords={this.props.coords} />
+      </div>
+    ) : (
+            <div>Getting the location data&hellip; </div>
+          );
+  }
 }
-
-export default App;
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(App);
---
+++ teamtwo/src/components/Weather.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Weather(props) {
+    console.log(props.coords);
+    return (
+        <>
+            <h2>Weather</h2>
+        </>
+    );
+}
